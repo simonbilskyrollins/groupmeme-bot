f210bb5daf1201065fc6750526ae960654bd9487
Simon Bilsky-Rollins <simonbr73@gmail.com>
2017-02-01 17:32:01 -0600

    Add standard and remove bad syntax

--- bot.js
+++ bot.js
@@ -1,242 +1,244 @@
-var HTTPS = require('https'),
-    request = require('request'),
-    fs = require('fs'),
-    ImageService = require('groupme').ImageService, // GroupMe image service wrapper
-    snoowrap = require('snoowrap');  // Reddit API wrapper
+var HTTPS = require('https')
+var request = require('request')
+var fs = require('fs')
+var ImageService = require('groupme').ImageService // GroupMe image service wrapper
+var Snoowrap = require('snoowrap')  // Reddit API wrapper
 
 // Get GroupMe bot ID
-var botID = process.env.BOT_ID;
+var botID = process.env.BOT_ID
 
 // Get Reddit API config
-try {
-  const r = new snoowrap({
-    userAgent: process.env.USER_AGENT,
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    refreshToken: process.env.REFRESH_TOKEN
-  });
-} catch(err) {
-  console.log("problem with Reddit API config");
-}
-
+var r = new Snoowrap({
+  userAgent: process.env.USER_AGENT || null,
+  clientId: process.env.CLIENT_ID || null,
+  clientSecret: process.env.CLIENT_SECRET || null,
+  refreshToken: process.env.REFRESH_TOKEN || null
+})
 
 // Array for regular expressions -> responses
-function getActionArr(){
-  return [ 
+function getActionArr () {
+  return [
     // fun warriors facts
     {
-      regex : new RegExp(".*\\bwarriors\\b.*", "i"),
-      action : function() {
-        botResponse = "Did you know that the Golden State Warriors blew a 3-1 lead in the 2016 NBA Finals?";
-        postMessage(botResponse);
+      regex: new RegExp('.*\\bwarriors\\b.*', 'i'),
+      action: function () {
+        var botResponse = 'Did you know that the Golden State Warriors blew a 3-1 lead in the 2016 NBA Finals?'
+        postMessage(botResponse)
       }
     },
 
     // talk about illnesses
     {
-      regex: new RegExp(".*\\bsick.*", "i"),
-      action : function() {
-        botResponse = "Too bad your immune system isn't as good as Steph's :(";
-        postMessage(botResponse);
+      regex: new RegExp('.*\\bsick.*', 'i'),
+      action: function () {
+        var botResponse = "Too bad your immune system isn't as good as Steph's :("
+        postMessage(botResponse)
       }
     },
 
     // Did I hear something about the patriarchy?
     {
-      regex: new RegExp(".*\\bpatriarch.*", "i"),
-      action: function() {
-        botResponse = "Fuck the patriarchy!";
-        postMessage(botResponse);
+      regex: new RegExp('.*\\bpatriarch.*', 'i'),
+      action: function () {
+        var botResponse = 'Fuck the patriarchy!'
+        postMessage(botResponse)
       }
     },
 
     // find wholesome memes
     {
-      regex : new RegExp(".*\\bmeme.*", "i"),
-      action : function() {
-        botResponse = "I hope this brightens your day";
-        getMeme('wholesomememes', function(imageUrl) {
-          postImageMessage(botResponse, imageUrl);
-        });
+      regex: new RegExp('.*\\bmeme.*', 'i'),
+      action: function () {
+        var botResponse = 'I hope this brightens your day'
+        getMeme('wholesomememes', function (imageUrl) {
+          postImageMessage(botResponse, imageUrl)
+        })
       }
     },
 
     // pull xkcd comics
     {
-      regex: new RegExp(".*\\b(xkcd|nerd|geek|dork|computer science).*", "i"),
-      action: function() {
-        botResponse = "";
-        getXkcd('', function(imageUrl, latestNum) {
-          var randomNum = Math.ceil(Math.random() * latestNum);
-          getXkcd(randomNum.toString(), function(imageUrl, num) {
-            postImageMessage(botResponse, imageUrl);
-          });
-        });
+      regex: new RegExp('.*\\b(xkcd|nerd|geek|dork|computer science).*', 'i'),
+      action: function () {
+        getXkcd('', function (xkcd) {
+          var randomNum = Math.ceil(Math.random() * xkcd.num)
+          getXkcd(randomNum.toString(), function (xkcd) {
+            var botResponse = 'Title: ' + xkcd.title + '\\nAlt-text: ' + xkcd.alt
+            postImageMessage(botResponse, xkcd.img)
+          })
+        })
       }
     }
-  ];
+  ]
 }
 
 // Called when a new message is sent to the group chat
-function respond(req, res) {
-  var request = req.body;
-  var botResponse, imageUrl;
+function respond (req, res) {
+  var request = req.body
 
   // Return a normal 200 status code
-  res.writeHead(200);
+  res.writeHead(200)
 
   // Matching logic
-  var matched = false;
-  getActionArr().forEach(function(actionResponse){
-    matched = performActionIfAppropriate(request, 
-      request.text && request.text.match(actionResponse.regex), 
-      actionResponse['action']) || matched;
-  });
+  var matched = false
+  getActionArr().forEach(function (actionResponse) {
+    matched = performActionIfAppropriate(request,
+      request.text && request.text.match(actionResponse.regex),
+      actionResponse['action']) || matched
+  })
 
   // log stuff we ignored
-  if (!matched){
-    console.log("don't care: ", request.text);
+  if (!matched) {
+    console.log("don't care: ", request.text)
   }
-  
-  res.end();
+
+  res.end()
 }
 
-function postMessage(botResponse, imageUrl) {
-  var options, body, botReq;
+function postMessage (botResponse, imageUrl) {
+  var options, body, botReq
 
   options = {
     hostname: 'api.groupme.com',
     path: '/v3/bots/post',
     method: 'POST'
-  };
+  }
 
   // Slightly different structure if we want to send an image
   if (imageUrl) {
     body = {
-      "bot_id" : botID,
-      "text" : botResponse,
-      "attachments" : [
+      'bot_id': botID,
+      'text': botResponse,
+      'attachments': [
         {
-          "type"  : "image",
-          "url"   : imageUrl
+          'type': 'image',
+          'url': imageUrl
         }
       ]
-    };
+    }
   } else {
     body = {
-      "bot_id" : botID,
-      "text" : botResponse
-    };
+      'bot_id': botID,
+      'text': botResponse
+    }
   }
 
-  console.log('sending ' + botResponse + ' to ' + botID);
+  console.log('sending ' + botResponse + ' to ' + botID)
 
   // actually post the message
-  botReq = HTTPS.request(options, function(res) {
-      if(res.statusCode == 202) {
+  botReq = HTTPS.request(options, function (res) {
+    if (res.statusCode === 202) {
         // neat
-      } else {
-        console.log('rejecting bad status code ' + res.statusCode);
-      }
-  });
+    } else {
+      console.log('rejecting bad status code ' + res.statusCode)
+    }
+  })
 
-  botReq.on('error', function(err) {
-    console.log('error posting message '  + JSON.stringify(err));
-  });
-  botReq.on('timeout', function(err) {
-    console.log('timeout posting message '  + JSON.stringify(err));
-  });
-  botReq.end(JSON.stringify(body));
+  botReq.on('error', function (err) {
+    console.log('error posting message ' + JSON.stringify(err))
+  })
+  botReq.on('timeout', function (err) {
+    console.log('timeout posting message ' + JSON.stringify(err))
+  })
+  botReq.end(JSON.stringify(body))
 }
 
 // postMessage wrapper that takes care of annoying image-posting details
-function postImageMessage(botResponse, imageUrl) {
+function postImageMessage (botResponse, imageUrl) {
   if (imageUrl) {
-    processImage(imageUrl, function(err, processedImageUrl) {
+    processImage(imageUrl, function (err, processedImageUrl) {
       if (err) {
-        return;
+        return
       } else {
-        console.log('attaching ' + processedImageUrl);
-        postMessage(botResponse, processedImageUrl);
+        console.log('attaching ' + processedImageUrl)
+        postMessage(botResponse, processedImageUrl)
       }
     })
   } else {
     botResponse = "Sorry, something went wrong and I'm fresh out of memes"
-    postMessage(botResponse);
+    postMessage(botResponse)
   }
 }
 
 // If we want to send an image, we first have to upload it to GroupMe's image
 // processing service
-function processImage(imageUrl, callback) {
-  var imageStream = fs.createWriteStream('tmp-image');
-  request(imageUrl).pipe(imageStream);
+function processImage (imageUrl, callback) {
+  var imageStream = fs.createWriteStream('tmp-image')
+  request(imageUrl).pipe(imageStream)
 
-  imageStream.on('close', function() {
+  imageStream.on('close', function () {
     ImageService.post(
       'tmp-image',
-          function(err,ret) {
+          function (err, ret) {
             if (err) {
-              console.log('error posting image ', imageUrl, 'to GroupMe');
-              callback(err, null);
+              console.log('error posting image ', imageUrl, 'to GroupMe')
+              callback(err, null)
             } else {
-              callback(null, ret.url);
+              callback(null, ret.url)
             }
-          });
-  });
+          })
+  })
 }
 
 // Get top link from given subreddit
-function getMeme(subreddit, callback) {
+function getMeme (subreddit, callback) {
   r.getSubreddit(subreddit).getHot({limit: 20}).filter(post => {
-    if (post.is_self==false && post.stickied==false && post.likes==null) {
-      return post;
-  }}).then(post => {
-    if (post.length > 0) {
-      meme = post[0]
-      r.getSubmission(meme.id).upvote();
-      console.log('retrieved meme ', meme.url);
-      callback(meme.url);
-    } else {
-      console.log('ran out of /r/wholesomememes posts');
-      callback();
+    if (post.is_self === false && post.stickied === false && post.likes == null) {
+      return post
     }
-  });
+  }).then(post => {
+    if (post.length > 0) {
+      var meme = post[0]
+      r.getSubmission(meme.id).upvote()
+      console.log('retrieved meme ', meme.url)
+      callback(meme.url)
+    } else {
+      console.log('ran out of /r/wholesomememes posts')
+      callback()
+    }
+  })
 }
 
 // Get an xkcd comic
-function getXkcd(number, callback) {
-  var BASE_URL = 'https://xkcd.com/';
+function getXkcd (number, callback) {
+  var BASE_URL = 'https://xkcd.com/'
+  var url
   if (number) {
-    url = BASE_URL + number + '/info.0.json';
+    url = BASE_URL + number + '/info.0.json'
   } else {
-    url = BASE_URL + 'info.0.json';
+    url = BASE_URL + 'info.0.json'
   }
   var options = {
     url: url,
     json: true
-  };
-  request(options, function(err, res, body) {
-    if (res.statusCode == 200) {
-      console.log('retrieved XKCD comic', body.num, body.img);
-      callback(body.img, body.num);
+  }
+  request(options, function (err, res, body) {
+    if (res.statusCode === 200) {
+      console.log('retrieved XKCD comic', body.num, body.img)
+      var xkcd = {
+        num: body.num,
+        img: body.img,
+        title: body.title,
+        alt: body.alt
+      }
+      callback(xkcd)
     } else {
-      console.log('error retrieveing XKCD comic');
+      console.log('error retrieveing XKCD comic:', err)
     }
-  });
+  })
 }
 
 /**
  * Helper method for detecting bot-response-worthy messages
  */
- function performActionIfAppropriate(request, isMatch, action){
-  if(isMatch && request.name && request.name !== "Beep Boop"){
-    action();
-    return true;
+function performActionIfAppropriate (request, isMatch, action) {
+  if (isMatch && request.name && request.name !== 'Beep Boop') {
+    action()
+    return true
   }
-  return false;
- }
+  return false
+}
 
-exports.respond = respond;
-exports.postMessage = postMessage;
-exports.postImageMessage = postImageMessage;
+exports.respond = respond
+exports.postMessage = postMessage
+exports.postImageMessage = postImageMessage
